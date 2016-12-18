8d13b91f6f1467a1a8d589fbb0d9fba3ebb324e1
AlexiaTorres <maikuxy20@gmail.com>
2016-12-18 19:21:32 +0100

    home

--- footer.php
+++ footer.php
@@ -1,9 +1,10 @@
 <?php
-	include 'legal.php';
-	include 'privacy.php';
-?>
-<div style="position: fixed; bottom: 0px!important;">
-	<span class="pull-left">
-		<a href="#modalLegal" role="button" data-toggle="modal" style="color: #050D2D;">Aviso legal</a> | <a href="#modalPrivacy" role="button" data-toggle="modal" style="color: #050D2D;">Política de privacidad</a>
-	</span>
-</div>
+//	include 'legal.php';
+//	include 'privacy.php';
+//?>
+<!--<div style="position: fixed; bottom: 0px!important;">-->
+<!--	<span class="pull-left">-->
+<!--		<a href="#modalLegal" role="button" data-toggle="modal" style="color: #555;">Aviso legal |</a> <a href="#modalPrivacy" role="button" data-toggle="modal" style="color: #555;">Política de privacidad</a>-->
+<!--	</span>-->
+<!--</div>-->
+
---
+++ rest.php
@@ -0,0 +1,243 @@
+
+
+<!-- WORK AND LEARN -->
+<section class="c-layout-revo-slider c-layout-revo-slider-2">
+    <div class="c-content-box c-size-md">
+        <div class="container">
+            <div class="c-content-client-logos-slider-1 c-bordered" data-slider="owl" data-items="6"
+                 data-desktop-items="4" data-desktop-small-items="3" data-tablet-items="3"
+                 data-mobile-small-items="2" data-auto-play="5000">
+                <div class="c-content-title-1 c-bg-white">
+                    <h3 class="c-center c-font-uppercase c-font-bold">Trabaja y aprende con las mejores
+                        herramientas</h3>
+                    <div class="c-line-center c-theme-bg">
+                    </div>
+                    <p style="margin: 1em;">
+                        Disfruta de la experiencia de trabajar con las nuevas <strong>Wacom Cintiq
+                            27QHD</strong>, la mejor herramienta para ilustradores y concept artist. Olvídate de
+                        las limitaciones tecnológicas y da rienda suelta a tu creatividad.
+                        El monitor interactivo, creativo y táctil Cintiq 27QHD brinda un nuevo estándar en color
+                        y resolución. Trabaja con la fluidez de la experiencia práctica a medida que creas con
+                        un lápiz sensible a la presión y gestos multitáctiles directamente en una amplia
+                        pantalla de vidrio sin bordes.
+                    </p>
+                </div>
+                <div class="item c-center">
+                    <img src="assets/base/img/content/ilustracion/wacom_logo.png" alt=""/>
+                </div>
+            </div>
+            <img class="infografia" alt="" src="assets/base/img/content/ilustracion/cintiq.png"
+                 style="opacity: 1!important;">
+        </div>
+    </div>
+</section>
+
+<!-- TEACHER -->
+<section class="c-layout-revo-slider c-layout-revo-slider-2" id="profesor">
+    <div class="c-content-box c-size-md">
+        <div class="container">
+            <div class="c-content-client-logos-slider-1 c-bordered" data-slider="owl" data-items="6"
+                 data-desktop-items="4" data-desktop-small-items="3" data-tablet-items="3"
+                 data-mobile-small-items="2" data-auto-play="5000">
+                <div class="c-content-title-1 c-bg-white">
+                    <h3 class="c-center c-font-uppercase c-font-bold">El profesor</h3>
+                    <div class="c-line-center c-theme-bg">
+                    </div>
+                    <h3 class="c-center">Nuestros profesores son profesionales altamente cualificados y con gran
+                        experiencia en el sector y la formación.</h3>
+                </div>
+                <div class="row">
+                    <div class="col-md-5">
+                        <div class="c-caption c-content-overlay">
+                            <div class="c-overlay-wrapper">
+                                <div class="c-overlay-content">
+                                    <button type="button"
+                                            class="btn btn-default c-btn-uppercase c-btn-square c-btn-uppercase">
+                                        <a class="teacher-portfolio" href="https://mvpurplespot.net"
+                                           target="_blank" data-lightbox="fancybox"
+                                           data-fancybox-group="gallery-1">Ver portfolio</a></button>
+                                </div>
+                            </div>
+                            <img class="img-responsive" src="assets/base/img/content/ilustracion/Valentina.PNG"
+                                 alt="">
+                        </div>
+                        <div class="c-bg-white">
+                            <div class="c-head text-muted">
+                                <div class="c-name c-font-uppercase c-font-bold" style="margin: 1em;">
+                                    Valentina Grases
+                                </div>
+                            </div>
+                            <div class="c-positiontext-muted" style="margin: 1em;">
+                                Concept Artist / Formadora de Ilustración y Concept Art
+                            </div>
+                            <p style="margin: 1em;">
+                                Especialista en ilustración, arte conceptual, cómic y animación 2D.
+                                <br/>Autora del comic online La ley del Zoul.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="col-md-7">
+                        <div class="cbp-item identity graphic">
+                            <div class="cbp-caption-defaultWrap">
+                                <img src="assets/base/img/content/ilustracion/sheron.jpg" alt=""
+                                     class="teacher-art">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</section>
+
+<!--FORMACION-->
+<section id="job">
+    <div class="c-content-box c-size-md">
+        <div class="container">
+            <div class="c-content-feature-2-grid" data-auto-height="true" data-mode="base-height">
+                <div class="row">
+                    <div class="col-md-4 col-sm-6">
+                        <div class="c-content-feature-2" data-height="height">
+                            <h3 class="c-font-bold advantages"><i class="fa fa-check-square-o advantages-icon"
+                                                                  aria-hidden="true"></i>Prácticas en España y
+                                UK</h3>
+                            Nuestra formación es complementada con <span class="strong">prácticas</span> en
+                            empresas nacionales e internacionales que demandan profesionales cualificados.
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6">
+                        <div class="c-content-feature-2" data-height="height">
+                            <h3 class="c-font-bold advantages"><i class="fa fa-check-square-o advantages-icon"
+                                                                  aria-hidden="true"></i>Formación totalmente
+                                práctica</h3>
+                            Aseguramos una formación <span class="strong">100% práctica</span>, donde el alumno
+                            estará trabajando continuamente sobre los conocimientos adquiridos.
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6">
+                        <div class="c-content-feature-2" data-height="height">
+                            <h3 class="c-font-bold advantages"><i class="fa fa-check-square-o advantages-icon"
+                                                                  aria-hidden="true"></i>Listo para el mercado
+                                laboral</h3>
+                            La formación está enfocada a preparar al alumno para enfrentarse al mundo laboral.
+                            Acabará su formación con un <span class="strong">proyecto final</span> que le
+                            servirá como carta de presentación en cualquier estudio.
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6">
+                        <div class="c-content-feature-2" data-wow-delay1="2s" data-height="height">
+                            <h3 class="c-font-bold advantages"><i class="fa fa-check-square-o advantages-icon"
+                                                                  aria-hidden="true"></i>Grupos reducidos</h3>
+                            Máximo 8 alumnos por clase, fomentando la interactividad. Con grupos de menos de 8,
+                            la formación se vuelve empírica y por tanto aumenta la velocidad de aprendizaje
+                            real.
+                        </div>
+                    </div>
+                    <div class="col-md-4 col-sm-6">
+                        <div class="c-content-feature-2" data-wow-delay1="2s" data-height="height">
+                            <h3 class="c-font-bold advantages"><i class="fa fa-check-square-o advantages-icon"
+                                                                  aria-hidden="true"></i>Financiación a tu
+                                medida</h3>
+                            En Aula Temática te queremos facilitar lo máximo posible el acceso a tu formación.
+                            Solicita tu plan personalizado de pago. Podemos ajustar, en función de la formación
+                            que elijas, un plan mensual de abono que se adapte a tu economía.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- CONTACT -->
+<section>
+    <div class="c-content-box c-size-md c-no-padding c-bg-img-center">
+        <div class="container">
+            <div class="c-content-feedback-1 c-option-2">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="c-container c-bg-white c-bg-img-bottom-right">
+                            <div class="c-content-title-1">
+                                <h3 class="c-font-uppercase c-font-bold">Búscanos en:</h3>
+
+                                <span><strong>La Escuela</strong></span>
+                                <br/><span>C/ Santa Engracia, 109
+                <br/><br/><span><strong>E-mail:</strong>
+                <br/><span>info@aulatematicamadrid.es
+                <br/><br/><span><strong>Teléfono:</strong>
+                <br/><span>913 196 688
+                <br/><br/><span><strong>WhatsApp:</strong>
+                <span>656 641 267
+                <br/><br/>
+
+                <div class="c-section">
+                  <ul class="c-content-iconlist-1 c-theme">
+                    <li>
+                      <a href="https://twitter.com/AulaTematica" target="_blank"><i class="fa fa-twitter"></i></a>
+                    </li>
+                    <li>
+                      <a href="https://www.facebook.com/EscuelaAulaTematica" target="_blank"><i
+                              class="fa fa-facebook"></i></a>
+                    </li>
+                    <li>
+                      <a href="https://www.instagram.com/aulatematica/" target="_blank"><i class="fa fa-instagram"></i></a>
+                    </li>
+                    <li>
+                      <a href="https://www.linkedin.com/company/aula-tem-tica" target="_blank"><i
+                              class="fa fa-linkedin"></i></a>
+                    </li>
+                    <li>
+                      <a href="https://www.youtube.com/channel/UCsELKXA1NspSk_MLt-GO4ig" target="_blank"><i
+                              class="fa fa-youtube"></i></a>
+                    </li>
+                  </ul>
+                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="c-contact">
+                            <div class="c-content-title-1" id="contact">
+                                <h3 class="c-font-uppercase c-font-bold">¿Quieres más información?</h3>
+                            </div>
+                            <form method="post" action="moreinfo-ilustracion.php">
+                                <div class="form-group">
+                                    <input name="author" type="text" placeholder="Nombre"
+                                           class="form-control c-square c-theme input-lg" required>
+                                </div>
+                                <div class="form-group">
+                                    <input name="email" type="text" placeholder="Email"
+                                           class="form-control c-square c-theme input-lg" required>
+                                </div>
+                                <div class="form-group">
+                                    <input name="phone" type="text" placeholder="Teléfono"
+                                           class="form-control c-square c-theme input-lg" required>
+                                </div>
+                                <div class="form-group">
+                                        <textarea name="info" rows="8" name="Mensaje"
+                                                  placeholder="¿Qué necesitas saber?"
+                                                  class="form-control c-theme c-square input-lg" required></textarea>
+                                </div>
+                                <div class="form-group">
+                                    <div class="col-sm-offset-4 col-md-8 form-buttons">
+                                        <input type="checkbox" required> Acepto la <a href="#modalPrivacy"
+                                                                                      role="button"
+                                                                                      data-toggle="modal">política
+                                            de privacidad</a>
+                                        <button type="submit"
+                                                class="btn c-btn-black c-btn-uppercase btn-lg c-btn-bold c-btn-square">
+                                            Enviar
+                                        </button>
+                                        <button type="reset"
+                                                class="btn btn-default c-btn-square c-btn-uppercase c-btn-bold">
+                                            Cancelar
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
