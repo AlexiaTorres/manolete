8cd89002c2f5edcd190af2f006105187fc3015c5
AlexiaTorres <maikuxy20@gmail.com>
2016-12-19 13:08:07 +0100

    include home, arrow go top red

--- index.php
+++ index.php
@@ -11,51 +11,8 @@ include 'cookies.php';
 <div class="c-layout-page">
 
     <div class="home-page-wrapper">
-        <section class="c-layout-revo-slider c-layout-revo-slider-2">
-            <div class="tp-banner-container c-theme tp-fullscreen tp-fullscreen-mobile">
-                <div class="tp-banner">
-                    <ul>
-                        <li data-transition="fade" data-slotamount="1" data-masterspeed="1000">
-                            <img alt="" src="assets/base/img/content/cover.jpg" data-bgposition="center center"
-                                 data-bgfit="cover" data-bgrepeat="no-repeat">
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </section>
-
-        <section class="c-layout-revo-slider c-layout-revo-slider-2">
-            <div class="c-content-box c-size-md">
-                <div class="container">
-                    <p>
-                        Manolete es una <span class="red-bold"> producción española al más puro estilo de los
-                        musicales de Broadway</span>. Un espectáculo en el que la ambientación y los efectos
-                        multimedia contrastan con cantaores, barítonos, tenores y mezzosopranos. Una fusión
-                        de estilos musicales. Una combinación armoniosa de lírica, flamenco, ballet y una
-                        orquesta de 35 instrumentos. Una obra destinada a recorrer el mundo transmitiendo
-                        arte, pasión y espectáculo.
-                        <br/><br/>
-                        Manolete es un musical que transporta al público a la España de la posguerra,
-                        <span class="red-bold">es un viaje a la década de los años 40</span>, que a lo largo de 2 horas
-                        narrará, a través
-                        de la vida de una de sus figuras más representativas (Manolete), la historia de una
-                        España de contrastes, de glamour y lujo en un país empobrecido por una guerra. <span
-                                class="red-bold">Una
-                        España en la que, a pesar de las necesidades, brilla con fuerza el arte, la
-                            valentía y la pasión</span>.
-                        <br/><br/>
-                        El protagonista de este espectáculo fue un ejemplo de disrupción, un símbolo
-                        internacional de libertad y autodeterminación. Su romance con Lupe Sino, así como la
-                        relación con su madre y una vida apasionada en todos los sentidos, generan un
-                        espectáculo musical de gran envergadura, dirigido a los amantes de la música, de la
-                        lírica y el flamenco, del arte y de la tradición. En definitiva, <span class="red-bold">un espectáculo
-                            dedicado a los amantes de la cultura y de la tradición en general</span>.
-                    </p>
-                </div>
-            </div>
-        </section>
-
         <?php
+        include "home.php";
         include "espectaculo.php";
         include "donde-y-cuando.php";
         include "productora-y-equipo.php";
